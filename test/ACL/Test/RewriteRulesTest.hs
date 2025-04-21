a2be0d8cb7b5d611578e6e4df96411423439ecf4
Théophile Choutri de Tarlé <theophile@choutri.eu>
2025-04-21 14:57:39 +0200

    Add custom roles tests

--- test/ACL/Test/RewriteRulesTest.hs
+++ test/ACL/Test/RewriteRulesTest.hs
@@ -3,6 +3,7 @@ module ACL.Test.RewriteRulesTest where
 import Test.Tasty
 
 import ACL.Test.RewriteRulesTest.BlocklistTest qualified as BlocklistTest
+import ACL.Test.RewriteRulesTest.CustomRolesTest qualified as CustomRolesTest
 import ACL.Test.RewriteRulesTest.FolderDocumentsTest qualified as FolderDocumentsTest
 import ACL.Test.RewriteRulesTest.MultipleRestrictionTest qualified as MultipleRestrictionTest
 import ACL.Test.RewriteRulesTest.OrgsPlansTest qualified as OrgsPlansTest
@@ -15,4 +16,5 @@ spec =
     , FolderDocumentsTest.spec
     , BlocklistTest.spec
     , MultipleRestrictionTest.spec
+    , CustomRolesTest.spec
     ]
---
+++ test/ACL/Test/RewriteRulesTest/CustomRolesTest.hs
@@ -0,0 +1,97 @@
+module ACL.Test.RewriteRulesTest.CustomRolesTest where
+
+import Data.Map (Map)
+import Data.Map.Strict qualified as Map
+import Data.Sequence qualified as Seq
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import ACL.Check
+import ACL.Test.Utils
+import ACL.Types.Namespace
+import ACL.Types.NamespaceId
+import ACL.Types.Object
+import ACL.Types.RelationTuple
+import ACL.Types.RewriteRule
+import ACL.Types.Subject
+
+spec :: TestTree
+spec =
+  testGroup
+    "Custom Roles"
+    [ testCase "Ann is assigned to role media-manager" (testThatAnnIsAMediaManager fixtures)
+    , testCase "Ann is an editor of asset-category:logos" (testThatAnnIsALogoEditor fixtures)
+    ]
+
+fixtures :: (Map NamespaceId Namespace, Set RelationTuple)
+fixtures =
+  let userNamespace = Namespace "user" Map.empty
+      assetCategoryNamespace =
+        let viewerRelation =
+              Union
+                ( Intersection
+                    (Single (This "user"))
+                    (Single ("assignee" `from` "role"))
+                )
+                (Single (ComputedSubjectSet "editor"))
+            editorRelation =
+              Single (This "user")
+         in Namespace
+              "asset-category"
+              ( Map.fromList
+                  [ ("viewer", viewerRelation)
+                  , ("editor", editorRelation)
+                  ]
+              )
+      roleNamespace =
+        let assigneeRelation = Single (This "user")
+         in Namespace
+              "role"
+              (Map.fromList [("assignee", assigneeRelation)])
+      namespaces =
+        Map.fromList
+          [ ("user", userNamespace)
+          , ("asset-category", assetCategoryNamespace)
+          , ("role", roleNamespace)
+          ]
+      mediaManagerRole = Object "role" "media-manager"
+      mediaViewerRole = Object "role" "media-viewer"
+      userAnn = Subject $ EndSubject "user" "ann"
+      userBeth = Subject $ EndSubject "user" "beth"
+      logosObject = Object "asset-category" "logos"
+      mediaManagers = SubjectSet $ SubjectSetTuple mediaManagerRole (Just "assignee")
+      mediaViewers = SubjectSet $ SubjectSetTuple mediaViewerRole (Just "assignee")
+      relationTuples =
+        Set.fromList
+          [ RelationTuple mediaManagerRole "assignee" userAnn
+          , RelationTuple mediaViewerRole "assignee" userBeth
+          , RelationTuple logosObject "editor" mediaManagers
+          , RelationTuple logosObject "viewers" mediaViewers
+          ]
+   in (namespaces, relationTuples)
+
+testThatAnnIsAMediaManager :: (Map NamespaceId Namespace, Set RelationTuple) -> Assertion
+testThatAnnIsAMediaManager (namespaces, relationTuples) = do
+  let mediaManagerRole = Object "role" "media-manager"
+      userAnn = Subject $ EndSubject "user" "ann"
+
+  aclResult <- assertRight "" =<< check namespaces relationTuples (mediaManagerRole, "assignee") userAnn
+
+  assertEqual
+    "Ann is not assigned to role media-manager!"
+    (True, Map.fromList [("direct", Seq.fromList ["_this"])])
+    aclResult
+
+testThatAnnIsALogoEditor  :: (Map NamespaceId Namespace, Set RelationTuple) -> Assertion
+testThatAnnIsALogoEditor (namespaces, relationTuples) = do
+  let logosObject = Object "asset-category" "logos"
+      userAnn = Subject $ EndSubject "user" "ann"
+
+  aclResult <- assertRight "" =<< check namespaces relationTuples (logosObject, "editor") userAnn
+
+  assertEqual
+    "Ann is not an editor of asset-category:logos!"
+    (True, Map.fromList [])
+    aclResult
